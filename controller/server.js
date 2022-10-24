fe15a07345ba7a1c1022cd9230f564f0abfd5750
mmohare2019 <mmohare@loyola.edu>
2022-10-23 21:31:30 -0400

    hello world server #24

--- controller/server.js
+++ controller/server.js
@@ -1,8 +1,27 @@
-const express = require("express");
-const app = express(); 
+/*
+const express = require ('express');
+const routes = require('./routes/tea'); // import the routes
+
+const app = express();
+
 app.use(express.json());
 
+
+app.use('/', routes); //to use the routes
+
 const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('App is listening on port ' + listener.address().port)
+    console.log('Your app is listening on port ' + listener.address().port)
+})
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
