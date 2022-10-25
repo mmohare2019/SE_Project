bb35c4cb3f297b86d3c0da0b0a816d9d8dd6bfa1
mmohare2019 <mmohare@loyola.edu>
2022-10-25 11:49:25 -0400

    fixed path problem #24

--- controller/server.js
+++ controller/server.js
@@ -1,8 +1,6 @@
 const express = require ('express');
 const routes = require('./routes/account'); // import the routes
 const path = require('path');
-//const bodyParser = require('body-parser');
-//app.use(bodyParser.urlencoded({extended: false}));
 
 const app = express();
 
