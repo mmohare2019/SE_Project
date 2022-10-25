da9876938a427a759cb77ded64f1c0a32d64ab19
mmohare2019 <mmohare@loyola.edu>
2022-10-24 21:09:04 -0400

    account request #24

--- controller/controllers/account.js
+++ controller/controllers/account.js
@@ -1,11 +1,18 @@
 // GET '/account'
 const getAccount = (req, res, next) => {
     res.json({message: "GET account"});
+
 }
 
 // POST '/account'
+// used to create a new account
 const postAccount = (req, res, next) => {
     res.json({message: "POST account"}); 
+
+    let account_type = req.body.account_type; 
+
+    // filter by account_type to enter into DB 
+
 }
 
 module.exports = {
