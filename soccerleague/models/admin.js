c122002563ec6ef8363081595b4f4299d5088528
mmohare2019 <mmohare@loyola.edu>
2022-10-25 22:48:28 -0400

    error checking #32 #31

--- soccerleague/models/admin.js
+++ soccerleague/models/admin.js
@@ -9,4 +9,16 @@ const AdminSchema = new Schema({
     password : {type: String, required: true}
 });
 
+// make sure name field isn't empty 
+AdminSchema.virtual("name").get(function () {
+    let fullname = "";
+    if (this.first_name && this.last_name) {
+      fullname = `${this.last_name}, ${this.first_name}`;
+    }
+    if (!this.first_name || !this.last_name) {
+      fullname = "";
+    }
+    return fullname;
+});
+
 module.exports = mongoose.model("Admin", AdminSchema);
--- soccerleague/models/parent.js
+++ soccerleague/models/parent.js
@@ -9,4 +9,16 @@ const ParentSchema = new Schema({
     password : {type: String, required: true}
 });
 
+// make sure name field isn't empty 
+ParentSchema.virtual("name").get(function () {
+    let fullname = "";
+    if (this.first_name && this.last_name) {
+      fullname = `${this.last_name}, ${this.first_name}`;
+    }
+    if (!this.first_name || !this.last_name) {
+      fullname = "";
+    }
+    return fullname;
+});
+
 module.exports = mongoose.model("Parent", ParentSchema);
