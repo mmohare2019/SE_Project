2fb88f0c9aa1f2adf62bc54134416cb97e6cd321
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-10-26 23:12:39 -0400

    added logo

--- soccerleague/app.js
+++ soccerleague/app.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Text, View } from "react-native";
 import HelloWorld from "./view/component/HelloWorld";
 import LoginScreen from "./view/screen/LoginScreen";
+import { StyleSheet, Text, View, Image } from 'react-native';
+ 
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+        <Image source = {require("./assets/soccerlogo.png")}/>
+      </View>
+    );
+  }
+}
 
 export default function App(){
     return (
