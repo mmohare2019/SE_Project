6faaaac812477b2d251240824acc3d18e5e4726c
Barbara-Kavanaugh <55026849+Barbara-Kavanaugh@users.noreply.github.com>
2022-10-27 12:06:30 -0400

    Update app.js

--- soccerleague/app.js
+++ soccerleague/app.js
@@ -11,7 +11,7 @@ var app = express();
 
 const mongoose = require("mongoose");
 /*
-const mongoDB = "mongodb+srv://Barbara-K-322:Loyola-2023@cluster0.lvagtqb.mongodb.net/SoccerApp?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://Barbara-K-322:Loyola-2023@cluster0.lvagtqb.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection; 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
