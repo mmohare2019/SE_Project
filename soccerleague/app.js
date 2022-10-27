736ff46389035a7226b67f56abb735ef68343ccf
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-10-26 23:24:29 -0400

    added color and styling #33

--- soccerleague/app.js
+++ soccerleague/app.js
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 import HelloWorld from "./view/component/HelloWorld";
 import LoginScreen from "./view/screen/LoginScreen";
 import { StyleSheet, Text, View, Image } from 'react-native';
- 
+
+// adding image
 export default class App extends React.Component {
   render(){
     return (
@@ -13,6 +14,22 @@ export default class App extends React.Component {
     );
   }
 }
+
+// styling with color and text
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D4D4D4D4',
+    alignItems: 'center',
+    justifyContent: 'center',
+   },
+ 
+   image :{
+    marginBottom: 35
+ 
+  }
+     
+});
 
 export default function App(){
     return (
