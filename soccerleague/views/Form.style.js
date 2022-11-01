3b420160a116d01977925f6933b4d00d90a290b5
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-01 00:57:27 -0400

    Finalized Form.style.js #33 #26

--- soccerleague/views/Form.style.js
+++ soccerleague/views/Form.style.js
@@ -1,15 +1,15 @@
 import { StyleSheet } from "react-native";
 export default StyleSheet.create({
-   //style for each screen
+   // style for each screen
     groupView:{ marginTop:50,marginLeft:8,marginRight:8},
     label:{paddingTop:10,fontSize:16,},
     input:{borderWidth: 1,borderRadius: 10,padding: 10,},
     formButton:{
         marginRight:5,marginLeft:5,marginTop:10,
-        paddingTop:10,paddingBottom:10,backgroundColor:'#ABD699',//app green color
+        paddingTop:10,paddingBottom:10,backgroundColor:'#ABD699',// app green color
         borderRadius:10,borderWidth: 1,borderColor: '#ABD699'
     },
     formButtonText:{
-        color:'black',textAlign:'center',fontSize:20,//black text on buttons
+        color:'black',textAlign:'center',fontSize:20,// black text on buttons
             paddingLeft: 10,paddingRight: 10},
     });
