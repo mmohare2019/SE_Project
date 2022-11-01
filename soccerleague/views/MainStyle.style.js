7b075d1d7ad28eb29753a50b64b1d2989c099a2b
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-01 01:00:35 -0400

    Finalized MainStyle.style.js #33 #26

--- soccerleague/views/MainStyle.style.js
+++ soccerleague/views/MainStyle.style.js
@@ -1,1 +1,17 @@
+import { StyleSheet } from "react-native";
 
+export default StyleSheet.create({
+    centerView: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center' 
+    },
+    emphasisText: {
+        fontSize: 40,
+        color:'black',
+        textAlign: 'center',
+    },
+    image: {
+         marginBottom: 40,
+    },
+ });
