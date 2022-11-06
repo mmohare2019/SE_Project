9dc49f91dbe3c27c25a90fbfdf5a677b6a9108af
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-06 12:57:24 -0500

    able to see front end

---
+++ soccerleague/views_react/view/MainStyle.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    centerView: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center' 
+    },
+    emphasisText: {
+        fontSize: 36,
+        color:'blue',
+        textAlign: 'center',
+    }
+ });
+ 
