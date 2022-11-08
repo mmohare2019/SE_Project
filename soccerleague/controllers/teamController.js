92eb3b2155c65d50cdd50eceea6edadb2cebd3a7
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-08 01:27:50 -0500

    #16 working on creating teams

--- soccerleague/controllers/teamController.js
+++ soccerleague/controllers/teamController.js
@@ -2,6 +2,22 @@ const Team = require("../models/teamDao");
 const { body, validationResult } = require("express-validator");
 var async = require("async");
 
-exports.create_team_post = (req, res, next) =>{
-    
-}
+// coach sets name and team colors 
+exports.create_team_post = [
+    body("team_name", "Team name required").trim().isLength({min: 1}).escape(),
+    body("color", "Color required").trim().isLength({min: 1}).escape(),
+
+    (req, res, next) => { 
+        // Extract validation errors from req
+        const errors = validationResult(req);
+
+        let newTeam = {};
+        newTeam.team_name = req.body.team_name;
+        newTeam.color = req.body.color;
+
+        Team.create(newTeam).then(function(result) {
+            res.json(result);
+        });
+
+    },
+];
