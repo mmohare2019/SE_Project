7338f74ca8fc416618a405f41d4cc746ebfcc57a
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-08 12:10:46 -0500

    #50 working on front end

--- soccerleague/views_react/view/screen/coachHome.js
+++ soccerleague/views_react/view/screen/coachHome.js
@@ -8,7 +8,35 @@ import axios from axios;
 const baseUrl = "http://10.0.2.2:3000";
 
 export default function CoachHome() {
+  async function updateTeam() {
+    const formData = new FormData(); 
+  }
+
+
+  async function admitPlayer() {
+    const formData = new FormData(); 
+  }
   
+  // View schedule 
+  async function viewSchedule() {
+  const formData = new FormData(); 
+  // request to endpoint to render other page 
+
+  }
+
+  // View standings
+  async function viewStandings() {
+  const formData = new FormData(); 
+  // request to endpoint to render other page 
+
+  }
+
+  // View playoff schedule 
+  async function viewPlayoff() {
+  const formData = new FormData(); 
+  // request to endpoint to render other page 
+
+  }
 }
 
 return ( <>
@@ -20,5 +48,41 @@ return ( <>
   <View style={FormStyle.groupView}>
     <Text style={MainStyle.emphasisText}> Coach Home </Text>
   </View>
+
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> updateTeam()}>
+      <Text style={FormStyle.formButtonText}> Edit team details </Text>
+    </TouchableOpacity>
+  </View>
+
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> admitPlayer()}>
+      <Text style={FormStyle.formButtonText}> Admit player </Text>
+    </TouchableOpacity>
+  </View>
+
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> viewSchedule()}>
+      <Text style={FormStyle.formButtonText}> View schedule </Text>
+    </TouchableOpacity>
+  </View>
+
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> viewStandings()}>
+      <Text style={FormStyle.formButtonText}> View standings </Text>
+    </TouchableOpacity>
+  </View>
+
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> viewPlayoff()}>
+      <Text style={FormStyle.formButtonText}> View playoff schedule </Text>
+    </TouchableOpacity>
+  </View>
+
 
 </> )
