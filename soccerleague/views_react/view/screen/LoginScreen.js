9fcf3378c56958843bf2f537580359bb4c73aa01
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-08 23:19:52 -0500

    Sign in SUCCESS!!

--- soccerleague/views_react/view/screen/LoginScreen.js
+++ soccerleague/views_react/view/screen/LoginScreen.js
@@ -3,6 +3,7 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import MainStyle from "../MainStyle.style";
 import FormStyle from "../Form.style";
 import axios from "axios";
+const QueryString = require('query-string');
 
 const baseUrl = "http://10.0.2.2:3000";
 
@@ -11,16 +12,26 @@ export default function LoginScreen(){
     const [password, setPassword] = React.useState("");
 
     async function handleSubmit(email, pswd){
+        /*
         const formData = new FormData();
         formData.append("email", email);
         formData.append("password", pswd);
 
         console.log(formData);
         console.log(`${baseUrl}/account/signin`);
+        */
 
         try {
             //const response = await axios.post(`${baseUrl}/account/signin`, formData);
-            const response = await axios.get(`${baseUrl}/account`);
+            //const response = await axios.get(`${baseUrl}/account/signin`);
+            const response = await axios.post(`${baseUrl}/account/signin`, QueryString.stringify ({
+                email: email,
+                password: password
+            }), {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                }
+            });
             console.log(response.data);
         } catch (error) {
             console.log(error.message);
