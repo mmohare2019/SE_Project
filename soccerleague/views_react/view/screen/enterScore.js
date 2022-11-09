228a14256d4e2d2902a39fda20471bcf94b1d711
dggillcrist <54948893+dggillcrist@users.noreply.github.com>
2022-11-09 12:33:39 -0500

    Create enterScore.js

---
+++ soccerleague/views_react/view/screen/enterScore.js
@@ -0,0 +1,32 @@
+import React from "react";
+import React, { useState } from "react";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import MainStyle from "../MainStyle.style";
+import FormStyle from "../Form.style";
+import { wnterScore } from "../component/homeScreen";
+import axios from axios;
+
+const baseUrl = "http://10.0.2.2:3000";
+
+export default function enterScore() 
+{ 
+  async function enterScore() 
+  {
+    const formData = new FormData();
+
+  }
+
+}
+
+return ( <>
+  <View style={FormStyle.groupView}>
+      <Text style={MainStyle.emphasisText}> Enter Score: </Text>
+  </View>
+  <View>
+    <TouchableOpacity style={FormStyle.formButton} 
+      onPress={()=> enterScore()}>
+      <Text style={FormStyle.formButtonText}> Submit Score </Text>
+    </TouchableOpacity>
+  </View>
+
+</>)
