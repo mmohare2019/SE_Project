6042c715e6f51ecf29554625c4c1a73a073d4916
dggillcrist <54948893+dggillcrist@users.noreply.github.com>
2022-11-09 20:07:45 -0500

    Update enterScore.js

--- soccerleague/views_react/view/screen/enterScore.js
+++ soccerleague/views_react/view/screen/enterScore.js
@@ -17,11 +17,27 @@ export default function enterScore()
   }
 
 }
-
+class Counter extends React.Component {
+    state = { count: 0 };
+    increment = () => this.setState({count: this.state.count + 1});
+    decrement = () => this.setState({count: this.state.count - 1});
+    render() {
+      
 return ( <>
   <View style={FormStyle.groupView}>
       <Text style={MainStyle.emphasisText}> Enter Score: </Text>
   </View>
+
+  <View style={styles.container}>
+    <TouchableOpacity onPress={this.decrement}>
+      <Text style={styles.text}>-</Text>
+    </TouchableOpacity>
+      <Text style={styles.text}>{this.state.count}</Text>
+    <TouchableOpacity onPress={this.increment}>
+      <Text style={styles.text}>+</Text>
+    </TouchableOpacity>
+  </View>
+
   <View>
     <TouchableOpacity style={FormStyle.formButton} 
       onPress={()=> enterScore()}>
