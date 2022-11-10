8b175a7f6c877227d8e63e20f24c79855dcb8861
Barbara-Kavanaugh <55026849+Barbara-Kavanaugh@users.noreply.github.com>
2022-11-10 14:11:54 -0500

    skeleton code for converting js schedule to react

--- soccerleague/in-progress-views/Schedule/React Schedule/src/App/App.js
+++ soccerleague/in-progress-views/Schedule/React Schedule/src/App/App.js
@@ -1,5 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const App= () => {
+    const [nav, setNav]= useState(0);
+    const [clicked, setClicked]= useState();
+    const [displayDate, setDisplayDate]= useState('');
+    const [days, setDays]= useState();
+    const [games, setGames]= useState(
+        localStorage.getItem('games') ? 
+        JSON.parse(localStorage.getItem('games')) : []);
+
+    const gameForDate= date =>games.find(g=> g.date=== date);
+
+    useEffect(() => {
+        localStorage.setItem('games', JSON.stringify(games));
+    }, [games]);
+
     return (<></>);
 };
