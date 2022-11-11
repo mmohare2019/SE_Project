c8b672b64349477bd6cf6d5d4a3788084912905a
Barbara-Kavanaugh <55026849+Barbara-Kavanaugh@users.noreply.github.com>
2022-11-10 23:31:20 -0500

    converting js team roster to react

---
+++ soccerleague/views_react/view/screen/TeamRoster/ReactTeamRoster/App.js
@@ -0,0 +1,22 @@
+import 'react' from React;
+import { StyleSheet, Text, View, Dimensions, FlatList, Image, SafeAreaView, TouchableOpacity } from 'react-native';
+
+const App= () =>
+{
+  return (
+    <SafeAreaView style= {styles.container}>
+      
+    </SafeAreaView>
+  );
+}
+
+export default App
+
+const styles= StyleSheet.create({
+  container: 
+  {
+    flex: 1,
+    paddingHorizontal: 10,
+    justify: 'center'
+  }
+})
