3fdcef1032c03bbb352afbf366d740ebf1f49fc3
Barbara-Kavanaugh <55026849+Barbara-Kavanaugh@users.noreply.github.com>
2022-11-10 21:23:22 -0500

    Update ScheduleHeader.jsx

--- soccerleague/views_react/view/screen/RegularSeasonSchedule/React Schedule/src/ScheduleHeader/ScheduleHeader.jsx
+++ soccerleague/views_react/view/screen/RegularSeasonSchedule/React Schedule/src/ScheduleHeader/ScheduleHeader.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
 
+export const ScheduleHeader= (nextNav, previousNav, displayDate) => {
+    return(
+        <div id="header">
+        <div id="displayMonth">{displayDate}</div>
+        
+        <div>
+            <button onClick= {previousNav} id="backButton">Previous</button>
+            <button onClick= {nextNav} id="nextButton">Next</button>
+        </div>
+    </div>
+    );
+};
