18d80a1262ce6e85a0984f2b60c1a2903d94632f
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-15 14:17:10 -0500

    finalized game controller for team score requests #14

--- soccerleague/controllers/gameController.js
+++ soccerleague/controllers/gameController.js
@@ -1,5 +1,30 @@
+const Game = require("../models/gameDao");
+const { body, validationResult } = require("express-validator");
 
 // views live game
 async function onViewLiveGame( ) {
   navigation.navigate('ViewScore');
 }
+
+exports.game_create_post = (req, res, next) => {
+    // extract validation errors from req
+    const errors = validationResult(req);
+     
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+  
+    // new game request for scores
+    const newGame = {
+      team1_score: req.body.team1_score,
+      team2_score: req.body.team2_score
+    };
+  
+    Game.create(newGame).then(function (result) {
+        res.json(result);
+      
+    }).catch((error) => {
+        res.status(400).json({error: error.array()});
+    });  
+
+};
