c906c9112f9117fcc8d03f3dd76f2f0911ca2e57
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-15 15:15:44 -0500

    finalized game router file #14 #15 #20

--- soccerleague/routes/game.js
+++ soccerleague/routes/game.js
@@ -1,1 +1,9 @@
+var express = require("express");
+var router = express.Router(); 
 
+const game_controller = require("../controllers/gameController");
+
+// CREATE GAME ROUTE //
+router.post("/", roster_controller.game_create_post);
+
+module.exports = router;
