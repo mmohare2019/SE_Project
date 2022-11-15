70489bec5aacf927cdab46fcea8806c5104fa427
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-15 13:28:31 -0500

    initialized game schemas #14 #15 #20

--- soccerleague/models/gameDao.js
+++ soccerleague/models/gameDao.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; 
 
+const GameSchema = new Schema ({
+    team1 : {type: Schema.Types.ObjectId, ref: "TeamSchema"}
+    team2 : {type: Schema.Types.ObjectId, ref: "TeamSchema"}
+});
+                               
