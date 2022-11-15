9cd88b63fa12e1914b1cc55f1a14ee7e07783595
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-11-15 13:33:49 -0500

    added baseline game model functions (w/o scores) #14 #15 #20

--- soccerleague/models/gameDao.js
+++ soccerleague/models/gameDao.js
@@ -5,4 +5,22 @@ const GameSchema = new Schema ({
     team1 : {type: Schema.Types.ObjectId, ref: "TeamSchema"}
     team2 : {type: Schema.Types.ObjectId, ref: "TeamSchema"}
 });
+
+const gameModel = mongoose.model("Game", GameSchema);
+
+exports.create = async function(newGame) {
+    const game = new gameModel(newGame);
+    const createdGame = await game.save(); 
+    return createdGame;
+}
+
+exports.deleteAll = async function() {
+    await gameModel.deleteMany(); 
+}
+
+exports.getAll = async function(req, res) {
+    res.status(200);
+    res.send(await gameModel.readAll());
+    res.end(); 
+}
                                
