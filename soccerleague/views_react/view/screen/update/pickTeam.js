3bc7584d168a963059b19ce86c7bd02ef6aafca4
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-26 22:43:12 -0500

    #16 able to get the teams

--- soccerleague/views_react/view/screen/update/pickTeam.js
+++ soccerleague/views_react/view/screen/update/pickTeam.js
@@ -5,28 +5,30 @@ import FormField from "../../component/formField";
 import FormStyle from "../../Form.style";
 import axios from "axios";
 import { response } from "express";
+import { get } from "mongoose";
 const QueryString = require('query-string');
 
 const baseUrl = "http://10.0.2.2:3000";
 
-export default function PickTeam({navigation, route}) {
- 
-    async function getTeams() {
-        console.log("Get teams");
-        try {
-            const response = await axios.get(`${baseUrl}/team/display`);
-            console.log(typeof response);
-            console.log("in try\t", response.data);  
-            team = response.data;
-    
-        } catch (error) {
-            console.log(error.message);
-        }
-    
-    }
+async function getTeams() {
+    try {
+        const response = await axios.get(`${baseUrl}/team/display`);
+        console.log(typeof response);
+        console.log("in try\t", response.data); 
+        return response.data;
 
-    getTeams();
-    console.log("My teams are\t", team);
+    } catch (error) {
+            console.log(error.message);
+    }
+}
+
+export default function PickTeam({navigation, route}) {
+    
+    let teams;
+    (async () => {
+        teams = await getTeams();
+        console.log("waiting..", teams);
+    })()
 
     async function handleSubmit() {
         navigation.navigate('PlayerHome');
