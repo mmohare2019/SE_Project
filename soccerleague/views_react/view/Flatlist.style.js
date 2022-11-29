6bfa4eb88a84ca4c8a69a37de6d988e2160febad
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-28 21:10:07 -0500

    #16 working on team sign up UI

---
+++ soccerleague/views_react/view/Flatlist.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+export default StyleSheet.create({ 
+  container: {
+    flex: 1,
+    //marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#d4d4d4',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
--- soccerleague/views_react/view/screen/update/pickTeam.js
+++ soccerleague/views_react/view/screen/update/pickTeam.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { TouchableOpacity, View, Text, Alert } from "react-native";
+import React, { Fragment } from "react";
+import { TouchableOpacity, View, Text, Alert, SafeAreaView, FlatList } from "react-native";
 import Header from "../../component/header";
 import FormField from "../../component/formField";
 import FormStyle from "../../Form.style";
+import FlatlistStyle from "../../Flatlist.style";
 import axios from "axios";
 import { response } from "express";
 import { get } from "mongoose";
+import { timeout } from "async";
 const QueryString = require('query-string');
 
 const baseUrl = "http://10.0.2.2:3000";
@@ -15,6 +17,7 @@ async function getTeams() {
         const response = await axios.get(`${baseUrl}/team/display`);
         console.log(typeof response);
         console.log("in try\t", response.data); 
+
         return response.data;
 
     } catch (error) {
@@ -22,14 +25,44 @@ async function getTeams() {
     }
 }
 
+
+const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      team_name: 'Team 1',
+      color: 'blue',
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      team_name: 'Team 2 ',
+      color: 'yellow',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      team_name: 'Team 3',
+      color: 'red',
+    },
+  ];
+
+const Item = ({ team_name, color }) => (
+    <View style={FlatlistStyle.item}>
+      <Text style={FlatlistStyle.text}> Team name: {team_name} </Text>
+      <Text style={FlatlistStyle.text}> Color: {color} </Text>
+    </View>
+);
+  
+
 export default function PickTeam({navigation, route}) {
-    
     let teams;
     (async () => {
         teams = await getTeams();
         console.log("waiting..", teams);
     })()
 
+    const renderItem = ({ item }) => (
+        <Item team_name={item.team_name} color={item.color}/>
+    );
+    
     async function handleSubmit() {
         navigation.navigate('PlayerHome');
     }
@@ -37,9 +70,18 @@ export default function PickTeam({navigation, route}) {
     return (<>
         <Header label="Pick a team to join"/>
 
+        <SafeAreaView style={FlatlistStyle.container}>
+        <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+        />
+        </SafeAreaView>        
+
         <TouchableOpacity style={FormStyle.formButton} 
             onPress={()=> handleSubmit()}>
             <Text style={FormStyle.formButtonText}> Submit </Text>
         </TouchableOpacity>
     </>)
+    
 }
