9005fb910bf83da8eb4eb5253da176f6085b4111
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-11-29 14:54:23 -0500

    #16 team display working

--- soccerleague/views_react/view/screen/update/pickTeam.js
+++ soccerleague/views_react/view/screen/update/pickTeam.js
@@ -8,75 +8,33 @@ const QueryString = require('query-string');
 
 const baseUrl = "http://10.0.2.2:3000";
 
-const TEAMS = [
-  {
-    _id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    team_name: 'Team 1',
-    color: 'blue',
-  },
-  {
-    _id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    team_name: 'Team 2',
-    color: 'yellow',
-  },
-  {
-    _id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    team_name: 'Team 3',
-    color: 'red',
-  },
-];
 
-const Item = ({ team_name, color }) => (
+const TeamDetails = ({ team_name, color }) => (
     <View style={FlatlistStyle.item}>
       <Text style={FlatlistStyle.text}> Team name: {team_name} </Text>
       <Text style={FlatlistStyle.text}> Color: {color} </Text>
     </View>
 );
-  
-var team;
 
 export default function PickTeam({navigation, route}) {
   const [team_list, setTeamList] = React.useState([]); 
 
-    //var teams;
-    /*
-    (async () => {
-        const TEAMS = await getTeams();
-        console.log("waiting..", TEAMS);
-    })()
-    */
-   /*
-    async function getTeams() {
-        try {
-            const response = await axios.get(`${baseUrl}/team/display`);
-            console.log(typeof response);
-            console.log("in try\t", response.data); 
-    
-            return response.data;
-    
-        } catch (error) {
-                console.log(error.message);
-        }
-    }
-    */
-
   function setData(data) {
     setTeamList(data); 
   }
+  
+  React.useEffect(() => {
+    axios.get(`${baseUrl}/team/display`).then((response) => {
+      const MY_TEAMS = response.data;
+      setData(MY_TEAMS);
+      console.log("in axios", MY_TEAMS);
+    })
+    .catch(error=> console.error(`Error: ${error}`));
+  }, []);
+  
 
-
-  axios.get(`${baseUrl}/team/display`).then((response) => {
-    const MY_TEAMS = response.data;
-    setData(MY_TEAMS);
-    console.log("in axios", MY_TEAMS);
-  })
-  .catch(error=> console.error(`Error: ${error}`));
-
-  //console.log(MY_TEAMS);
-
-  //console.log("my teams", MY_TEAMS);
   const renderItem = ({ item }) => (
-    <Item team_name={item.team_name} color={item.color}/>
+    <TeamDetails team_name={item.team_name} color={item.color}/>
   );
     
   async function handleSubmit() {
