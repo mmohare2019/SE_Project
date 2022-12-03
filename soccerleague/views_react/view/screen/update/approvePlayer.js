c9b4177dbc95468f378ecc6f2e1c404c322f892d
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-12-03 14:25:05 -0500

    #16 player info displaying

--- soccerleague/views_react/view/screen/update/approvePlayer.js
+++ soccerleague/views_react/view/screen/update/approvePlayer.js
@@ -7,32 +7,45 @@ import axios from "axios";
 const QueryString = require('query-string');
 const baseUrl = "http://10.0.2.2:3000";
 
-
 function onPress() {
     console.log("Hello world");
 }
 
+function extractPlayerIds(pendings) {
+    var user_ids = []; 
+    for (var i = 0; i < pendings.length; i++) {
+        user_ids.push(pendings[i].player);
+    }
+    return user_ids;
+} 
+
 export default function ApprovePlayer({navigation, route}) {
     const [player_list, setPlayerList] = React.useState([]); 
-
     const team = route.params;
-    console.log("Team in approve is", team);
 
-    async function getPlayerInfo(pendings) {
-       var user_ids = [];
-        for (var i = 0; i < pendings.length; i++) {
-            user_ids.push(pendings[i].player);
-        }
+    React.useEffect(() => {
+        axios.post(`${baseUrl}/pending/list`, {
+            team: team
+        }).then(function (response) {
+            console.log(response.data);
 
-        axios.post(`${baseUrl}/account/lookup`, {
-            user_ids: user_ids
-        }).then(function(response) {
-            console.log("response.data in getPlayerInfo", response.data);
-            return response.data;
-        }).catch(function (error) {
+            const user_ids = extractPlayerIds(response.data);
+
+            axios.post(`${baseUrl}/account/lookup`, {
+                user_ids: user_ids
+            }).then(function(response) {
+                console.log("response.data in getPlayerInfo", response.data);
+                setPlayerList(response.data);
+
+            }).catch(function (error) {
+                console.log(error);
+            });
+        })
+        .catch(function (error) {
             console.log(error);
         });
-    }
+
+    }, []); 
 
     const renderItem = ({ item }) => (
         <PlayerDisplay 
@@ -43,22 +56,6 @@ export default function ApprovePlayer({navigation, route}) {
         />
     )
 
-    // make this async 
-    React.useEffect(() => {
-        axios.post(`${baseUrl}/pending/list`, {
-            team: team
-      
-        }).then(async function (response) {
-            console.log("Players awaiting approval are", response.data);
-            var display = await getPlayerInfo(response.data);
-            console.log("display: ", display);
-            setPlayerList(display);
-      
-        }).catch(function (error) {
-            console.log(error);
-        });
-    }, []);
-    
     return (<>
         <Header label="Review the following player requests:" />
 
@@ -68,6 +65,6 @@ export default function ApprovePlayer({navigation, route}) {
             renderItem={renderItem}
             keyExtractor={item => item._id}
         />
-        </SafeAreaView>     
+        </SafeAreaView>   
     </>)
 }
