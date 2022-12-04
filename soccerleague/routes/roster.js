f3ad9856621767e293ed4cc3a550e5190eb15835
mmohare2019 <55029888+mmohare2019@users.noreply.github.com>
2022-12-04 12:32:16 -0500

    removing model not used

--- soccerleague/routes/roster.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router(); 
-
-const roster_controller = require("../controllers/rosterController");
-
-// CREATE ROSTER ROUTE //
-router.post("/", roster_controller.roster_create_post);
-
-module.exports = router;
