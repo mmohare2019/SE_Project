bad15c936b8043eb8893c8f4ee5fd699145e9869
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-12-05 22:09:08 -0500

    Corrected import lines #14

--- soccerleague/views_react/view/screen/view/viewScore.js
+++ soccerleague/views_react/view/screen/view/viewScore.js
@@ -3,5 +3,6 @@ import React, { useState } from "react";
 import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 import MainStyle from "../MainStyle.style";
 import FormStyle from "../Form.style";
-import { enterScore } from "../component/homeScreen";
+import Timer from "../../../component/timer";
+import { enterScore } from "../../../component/homeScreen";
 import axios from axios;
