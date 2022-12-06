1d0bb73a296b717b2a1f5c03c82ae3df3b282f9b
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-12-05 22:02:07 -0500

    Finalized pre-testing timer component for games #14

--- soccerleague/views_react/view/component/timer/timer.js
+++ soccerleague/views_react/view/component/timer/timer.js
@@ -1,1 +1,34 @@
+import {useState,useEffect} from 'react'
+import {View,StyleSheet} from 'react-native';
+import TimeControls from './timeControls';
+import TimeDisplay from './timeDisplay';
 
+export default function Timer(){
+    const [time,setTime] = useState(0);
+    const [status,setStatus] = useState(-1)
+    const reset=()=>{
+    }
+    const handleStart=()=>{
+    }
+    const handleStop=()=>{
+    }
+    return(
+        <View style={styles.container}>
+            <TimeDisplay time={time} />
+            <Controls
+                status={status}
+                handleStart={handleStart}
+                handleStop={handleStop}
+            />
+        </View>
+    )
+}
+const styles= StyleSheet.create({
+    container:{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'center',
+        justifyContent: 'center',
+        width: '100%'
+    },
+})
