d2d138e8848e81df6f93a176e63ac25316167405
mitchb25j <54994940+mitchb25j@users.noreply.github.com>
2022-12-06 10:00:56 -0500

    added get info func #15

--- soccerleague/models/standingsDao.js
+++ soccerleague/models/standingsDao.js
@@ -11,8 +11,16 @@ QuestionSchema.plugin(validateInteger);
 
 const standingsModel = mongoose.model("Standings", StandingsSchema);
 
+// creation function
 exports.create = async function(newStandings) {
     const standings = new standingsModel(newStandings);
     const createdStandings = await standings.save(); 
     return createdStandings;
+}
+
+// get all retrieval function
+exports.getAll = async function(req, res) {
+    res.status(200);
+    res.send(await standingsModel.readAll());
+    res.end(); 
 }
